04a183fea049ff45d479bbcbfbebfc25aeab3d38
John Vincent Basto <63955817+jvincentbasto@users.noreply.github.com>
2021-06-25 23:40:53 +0800

    Update Hero.vue

--- src/views/homepage/Hero.vue
+++ src/views/homepage/Hero.vue
@@ -153,7 +153,7 @@ export default {
 .model {
   &-img {
     box-shadow: 0 0px 4px rgba(black, 0.5), 0 4px 4px rgba(black, 0.5);
-    --num: '#{var(--c-neg)}5%';
+    --num: calc(var(--c-neg) * 5%);
     background: styles.fns-darken(var(--c-lprimary), var(--num));
   }
 }
