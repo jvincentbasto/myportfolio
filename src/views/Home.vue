ef195e75e9fc3bf9b3a68a1e30dd4b4bc7c50076
John Vincent Basto <63955817+jvincentbasto@users.noreply.github.com>
2021-06-26 00:34:02 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,28 @@
 <template>
   <navbar :data="data" />
   <Hero />
-  <!-- <About /> -->
-  <!-- <Skills /> -->
-  <!-- <Projects /> -->
-  <!-- <my-footer /> -->
+  <About />
+  <Skills />
+  <Projects />
+  <my-footer />
 </template>
 
 <script>
 import Navbar from "@/components/layouts/navbar.vue";
 import Hero from "./homepage/Hero.vue";
-// import About from "./homepage/About.vue";
-// import Skills from "./homepage/Skills.vue";
-// import Projects from "./homepage/Projects.vue";
-// import MyFooter from "@/components/layouts/footer.vue";
+import About from "./homepage/About.vue";
+import Skills from "./homepage/Skills.vue";
+import Projects from "./homepage/Projects.vue";
+import MyFooter from "@/components/layouts/footer.vue";
 
 export default {
   components: {
     Navbar,
     Hero,
-    // About,
-    // Skills,
-    // Projects,
-    // MyFooter,
+    About,
+    Skills,
+    Projects,
+    MyFooter,
   },
   setup() {
     const links = [
